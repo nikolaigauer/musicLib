b801cd6ee2af2d5d3c4477a5304c040fb7721ead
Nikolai <nikolaigauer@gmail.com>
2017-07-31 00:56:46 +0000

    Fix bad code so testing and console loggin works

--- musicLib.js
+++ musicLib.js
@@ -1,20 +1,33 @@
 class Library {
-  constructor (name, creator) => {
+  constructor (name, creator) {
   this.name = name;
   this.creator = creator;
   this.playlists = [];
   }
 }
 
+const testLibrary = new Library("The Name", "Coolio")
+console.log(testLibrary)
+
 class Playlist {
-  constructor (name) => {
+  constructor (name) {
     this.name = name;
-    this.tracks [];
+    this.tracks = [];
+  }
+  // overallRating () => {
+  //   let sum = 0;
+
+  // }
+  totalDuration() {
+    this.tracks.forEach(() => {
+      sumOfLength += track.length;
+    })
+    return sumOfLength;
   }
 }
 
 class Track {
-  constructor (title, rating, length) => {
+  constructor (title, rating, length) {
     this.title = title;
     this.rating = rating;
     this.length = length;
