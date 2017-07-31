42cdd4c044df83b5a183d17216c9aeb64b5aa776
Nikolai <nikolaigauer@gmail.com>
2017-07-31 00:43:07 +0000

    Add playlist and track constructors

--- musicLib.js
+++ musicLib.js
@@ -6,4 +6,17 @@ class Library {
   }
 }
 
+class Playlist {
+  constructor (name) => {
+    this.name = name;
+    this.tracks [];
+  }
+}
 
+class Track {
+  constructor (title, rating, length) => {
+    this.title = title;
+    this.rating = rating;
+    this.length = length;
+  }
+}
