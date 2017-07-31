df21d7e4f48c2b00daf8232946287d459c6e97e6
Nikolai <nikolaigauer@gmail.com>
2017-07-31 01:54:14 +0000

    Add basic working library constructor, needs work on the duration and rating functions

--- musicLib.js
+++ musicLib.js
@@ -14,24 +14,39 @@ class Playlist {
   constructor (name) {
     this.name = name;
     this.tracks = [];
+    // this.rating = []; overallRating(this.tracks.rating)
+    this.duration = [];
   }
+
+// attempt to push number of tracks as number as a total to list in either playlist or library
+  // numberOfTracks(track) {
+  //   let sumTracks = 0;
+  //   this.tracks.forEach((track) => {
+  //     sumTracks += track;
+  //   })
+  //   this.tracks.push(sumTracks)
+  // }
 
   overallRating() {
     let sumRating = 0;
-      this.tracks.forEach((track) => {
+    this.tracks.forEach((track) => {
         sumRating += track.rating;
       });
       return sumRating;
-
   }
 
+// this needs work
   totalDuration() {
     let sumLength = 0
     this.tracks.forEach((track) => {
       sumLength += track.length;
     })
     return sumLength;
+    duration.push(sumLength)
+
   }
+
+
 }
 
 const testPlaylist = new Playlist("Best Playlist");
@@ -51,4 +66,6 @@ const roygbiv = new Track("Roygbiv", 5, 2.5);
 const ritournelle = new Track("La Ritournelle", 4, 7.5)
 // console.log(roygbiv, ritournelle)
 testPlaylist.tracks.push(roygbiv, ritournelle);
+testLibrary.playlists.push(testPlaylist);
 console.log(testPlaylist);
+console.log(testLibrary)
